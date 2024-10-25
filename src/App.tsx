1a86623cc21cda75627e50b091c4d6a74503f31f
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-10-25 16:11:58 +0200

    achieved the fourth quests

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import PokemonCard from "./components/PokemonCard.tsx";
 function App() {
 	return (
 		<div>
-			<PokemonCard />
+			<PokemonCard pokemon={pokemonList[0]} />
 		</div>
 	);
 }
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "Mew",
+	},
+];
 
 export default App;
